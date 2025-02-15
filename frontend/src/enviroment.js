e2620db01d1f29c666155c4ef5510ad32b8287d3
Abhi198511Maurya <abhishekkumarmaurya1545@gmail.com>
2025-02-16 00:13:52 +0530

    varify the production setup

--- frontend/src/enviroment.js
+++ frontend/src/enviroment.js
@@ -1,4 +1,4 @@
 let IS_PROD = true;
-const server = IS_PROD ? "https://meetsync-jbe2.onrender.com" : "http://localhost:8080"
+const server = IS_PROD ? "https://meetsyncbackend.onrender.com" : "http://localhost:8080"
 
 export default server;
